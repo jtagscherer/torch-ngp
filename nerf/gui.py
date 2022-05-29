c969c5cab94814ed9e09a6a7ccfb39e37a876043
Jan Tagscherer <jan.tagscherer@gmail.com>
2022-05-29 18:05:42 +0200

    Slow down camera motion

--- nerf/gui.py
+++ nerf/gui.py
@@ -118,7 +118,7 @@ class NeRFGUI:
     def test_step(self):
         # TODO: seems we have to move data from GPU --> CPU --> GPU?
         self.render_step += 1
-        self.cam.orbit(math.sin(self.render_step / 1000.0) * 4, math.cos(self.render_step / 1000.0) * 4)
+        self.cam.orbit(math.sin(self.render_step / 1000.0) * 2, math.cos(self.render_step / 1000.0) * 2)
         self.need_update = True
 
         if self.need_update or self.spp < self.opt.max_spp:
