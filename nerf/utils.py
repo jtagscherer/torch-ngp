dde143fa0ba5813c14220acace59716287da4b33
Jan Tagscherer <jan.tagscherer@gmail.com>
2022-06-02 18:47:48 +0200

    Stop rendering depth maps as images

--- nerf/utils.py
+++ nerf/utils.py
@@ -438,7 +438,7 @@ class Trainer(object):
                 prediction = outputs['image']
                 prediction_depth = outputs['depth']
 
-                if self.global_step < style_training_start_step + 100:
+                '''if self.global_step < style_training_start_step + 100:
                     # Render predictions and depth maps
                     pred_image = prediction.detach()
                     pred_image = pred_image.reshape(67, 81, 3).permute(2, 0, 1).contiguous()
@@ -447,7 +447,7 @@ class Trainer(object):
                     torch_vis_2d(pred_image)
                     plt.savefig(f'/tmp/nerfout/{self.global_step}_pred.png')
                     torch_vis_2d(depth_image)
-                    plt.savefig(f'/tmp/nerfout/{self.global_step}_depth.png')
+                    plt.savefig(f'/tmp/nerfout/{self.global_step}_depth.png')'''
 
                 ground_truth = gt_rgb
 
